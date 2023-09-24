05acf1ffebf9fff03cafe3509f969f5bdd286e4e
Leonardo Ramos Anacleto <135238872+lranacleto85@users.noreply.github.com>
2023-09-24 15:36:27 -0300

    Exercícios e Desafios de vetores

---
+++ Exemplo2_vetor.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+ int qtd;
+    printf("\n Qual tamanho do vetor:");
+    scanf("%d", &qtd);
+    int vet[qtd], i;
+    int numero;
+    printf("\n informe um número inteiro: ");
+    scanf("%d", &numero);
+    for (i = 0; i < qtd; i++);
+    {
+        vet[i] = numero - ;
+        printf("\n %d", vet[i]);
+        numero-=1;
+    }
+        for (i=0, i<4: i++);
+        media+=vet[1];
+    }
+    
+    
+  return EXIT_SUCCESS;
+}
